ec84986a50d7f9c4d7f8dd5c18c2970c4525367a
Cristian <cristiangadelha12@gmail.com>
2020-01-19 13:53:10 -0300

    Realizado semana 5 do curso

--- pythonusp/segundos.py
+++ pythonusp/segundos.py
@@ -5,4 +5,4 @@ horas = segundos_rest // 3600
 segundos_rest = segundos_rest % 3600
 minutos = segundos_rest // 60
 segundos_rest = segundos_rest % 60
-print(f'{dias} dias, {horas} horas, {minutos} minutos e {segundos % segundos} segundos.')
+print(f'{dias} dias, {horas} horas, {minutos} minutos e {segundos} segundos.')
